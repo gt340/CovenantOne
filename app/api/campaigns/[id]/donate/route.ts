cd8091b995e497287fdee087a0c5d2f622eecb37
Kodiya Nekara <nekarakodiya@gmail.com>
2026-09-25 06:27:26 +0000

    Fix import path: @/lib/paymentProvider not @/src/lib (tsconfig maps @/* to ./src/*)

--- app/api/campaigns/[id]/donate/route.ts
+++ app/api/campaigns/[id]/donate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import { createServerClient } from "@supabase/ssr";
-import { isPaymentProviderConfigured, isTestMode, initializeTransaction } from "@/src/lib/paymentProvider";
+import { isPaymentProviderConfigured, isTestMode, initializeTransaction } from "@/lib/paymentProvider";
 
 async function getAuthedClientAndUser() {
   const cookieStore = await cookies();
